c81bc9c6960a655b8ffa8269e378c7a30e16295a
Dominik Spengler <dspengle@hsr.ch>
2011-03-05 16:07:30 +0100

    initial android project setup

---
+++ src/ch/hsr/eyecam/eyeCamActivity.java
@@ -0,0 +1,13 @@
+package ch.hsr.eyecam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class eyeCamActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
