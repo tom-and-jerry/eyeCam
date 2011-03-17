29d32726bc79e46c4c7db78cde9d9960b9dd9f0c
Patrice Mueller <pmueller@hsr.ch>
2011-03-17 01:23:25 +0100

    bugFix Refactored, TODO: optimalSize- and optimalFrameRate method

--- src/ch/hsr/eyecam/EyeCamActivity.java
+++ src/ch/hsr/eyecam/EyeCamActivity.java
@@ -1,94 +1,27 @@
 package ch.hsr.eyecam;
 
-import java.io.IOException;
-
 import android.app.Activity;
 import android.content.Context;
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.os.PowerManager;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.os.PowerManager.WakeLock;
 
 
 
 public class EyeCamActivity extends Activity {
-	private Camera mCamera;
-	private SurfaceHolder mHolder;
-	private SurfaceView mSurfaceView;
-	private PowerManager.WakeLock mWakeLock;
-	private static final String TAG = "EyeCamActivity";
-	
-	private SurfaceHolder.Callback mCallback = new SurfaceHolder.Callback() {
+	//private PowerManager.WakeLock mWakeLock;
 
-		public void surfaceDestroyed(SurfaceHolder holder) {
-			if (mCamera != null){
-				mCamera.stopPreview();
-				releaseCamera();
-			}
-		}
-
-		public void surfaceCreated(SurfaceHolder holder) {
-			setupCamera();
-		}
-
-		public void surfaceChanged(SurfaceHolder holder, int format, int width,
-				int height) {
-			setupHolder(holder);
-		}
-	};
-
-	private DisplayMetrics getDisplaySize() {
-		DisplayMetrics outMetrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(outMetrics);
-		return outMetrics; 
-	}
-
-	private void setupHolder(SurfaceHolder holder) {
-		mHolder = holder;
-		mHolder.addCallback(mCallback);
-		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-	}
-
-	private void setupCamera() {
-		try {
-			mCamera.setPreviewDisplay(mHolder);
-			Parameters params = modifyCamParameter(mCamera.getParameters());
-			mCamera.setParameters(params);
-		} catch (IOException e) {
-			mCamera.release();
-			Log.v(TAG,e.getMessage());
-		}
-	}
-
-	private Parameters modifyCamParameter(Parameters params) {		
-		params.setPreviewSize(getDisplaySize().widthPixels, getDisplaySize().heightPixels);
-		return params;
-	}
-
-	private void releaseCamera() {
-		mCamera.release();
-		mCamera = null;
-	}
+	private WakeLock mWakeLock;
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		releaseCamera();
 		mWakeLock.release(); 
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-
-		mCamera = Camera.open();
-		setupCamera();
-		mCamera.startPreview();
-		
 		mWakeLock.acquire();
 	}
 
@@ -101,10 +34,8 @@ public class EyeCamActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.main);
-	
-		mSurfaceView = (SurfaceView) findViewById(R.id.cameraSurface);
-		setupHolder(mSurfaceView.getHolder());
 		
 		PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
 		mWakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "eyeCam");
--- src/ch/hsr/eyecam/EyeCamSurfaceView.java
+++ src/ch/hsr/eyecam/EyeCamSurfaceView.java
@@ -1,64 +1,94 @@
 package ch.hsr.eyecam;
 
+
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.Camera.Size;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class EyeCamSurfacView extends SurfaceView implements SurfaceHolder.Callback {
+import java.io.IOException;
+import java.util.List;
 
+public class EyeCamSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+	private Camera mCamera;
 	private SurfaceHolder mHolder;
 	private final DisplayMetrics mMetrics = new DisplayMetrics();
-	private Camera mCamera;
 	private final static String mTAG = "ch.hsr.EyeCamSurfaceView";
 	
-	public EyeCamSurfacView(Context context, AttributeSet attrs) {
+	public EyeCamSurfaceView(Context context){
+		super(context);
+		initHolder();
+	}
+	public EyeCamSurfaceView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		((Activity) context).getWindowManager()
         .getDefaultDisplay().getMetrics(mMetrics );
 		
+		initHolder();
+	}
+
+	private void initHolder() {
 		mHolder = getHolder();
 		mHolder.addCallback(this);
 		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	}
 	
 	private void setupCamera() {
-		Camera.Parameters parameters = mCamera.getParameters();
-		parameters.setPictureSize(mMetrics.widthPixels, mMetrics.heightPixels);
+		Camera.Parameters parameters = mCamera.getParameters();	
+		Size optSize = getOptimalSize(parameters.getSupportedPreviewSizes());
+		parameters.setPreviewSize(optSize.width, optSize.height);
 		mCamera.setParameters(parameters);
 	}
 	
 	private void initCamera() {
 		if(mCamera != null) return;
+		mCamera = Camera.open();
 		try{
 			mCamera.setPreviewDisplay(mHolder);
-		} catch (Exception e) {
+		} catch (IOException e) {
 			releaseCamera();
 			Log.v(mTAG,e.getCause()+e.getMessage());
 		}
 		
 	}
 	
+	private Size getOptimalSize(List<Size> sizeList){
+		Size optSize= null;
+		for(Size size:sizeList){
+			if(mMetrics.heightPixels == size.height
+					&& mMetrics.widthPixels == size.width)
+				optSize = size;
+		}
+		return optSize;
+	}
+	
 	private void releaseCamera(){
+		if(mCamera == null) return;
 		mCamera.release();
 		mCamera = null;
 	}
 	
-	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
 		initCamera();
 		setupCamera();
 		mCamera.startPreview();
 		
 	}
 	
+	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		initCamera();
 	}
 	
+	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		mCamera.stopPreview();
 		releaseCamera();
